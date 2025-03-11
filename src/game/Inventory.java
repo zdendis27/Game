dbd4d12d2077bfccfd4eb3d29d62fc2f80d73768
zdendis27 <vacek4@spsejecna.cz>
2025-03-11 20:11:32 +0100

    zkouska

--- src/game/Inventory.java
+++ src/game/Inventory.java
@@ -1,4 +1,15 @@
 package game;
 
+import java.util.ArrayList;
+
 public class Inventory {
+    private ArrayList<Item> inventory = new ArrayList<>();
+
+    public boolean addItem(){
+        return true;
+    }
+
+    public boolean removeItem(){
+        return true;
+    }
 }
